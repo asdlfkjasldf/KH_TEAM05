0f189a631c0c7e211cdd260231c5af04176d5f97
JungHoon-Seong <criticalasia@naver.com>
2021-10-14 14:15:43 +0900

    회원관리의 페이징 기능을 컨텍스트루트가 변경되더라도 작동하도록 보완하였다

--- src/com/indimoa/manage/member/controller/MemberManagementListServlet.java
+++ src/com/indimoa/manage/member/controller/MemberManagementListServlet.java
@@ -35,9 +35,11 @@ public class MemberManagementListServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String searchCondition = request.getParameter("sc");  //select 옵션 중에 고른 조건 
-		String searchKeyword = request.getParameter("sk");  //input text에 들어간 키워드
-//		if(!(searchKeyword.equals(""))) {
+		String searchCondition = request.getParameter("sc");  //select 옵션 중에 고른 조건
+		
+		String searchKeyword = "";
+		searchKeyword = request.getParameter("sk");  //input text에 들어간 키워드
+		
 		Member vo = new Member(searchCondition, searchKeyword);
 		if (vo != null) {
 		response.setCharacterEncoding("UTF-8");
@@ -64,11 +66,9 @@ public class MemberManagementListServlet extends HttpServlet {
 //		out.println("입력된 title: "+ searchCondition); 작동확인
 //		out.println("<br>입력된 content: "+ searchKeyword);
 
-//			System.out.println("vo : "+ vo);
-			ArrayList<Member> searchvolist = new MemberManagementListService().selectBoardList(vo);
-//			System.out.println("searchvolist : " + searchvolist);	//검색 제대로 작동확인
-			request.setAttribute("boardvolist", searchvolist);
+
 			
+				
 			// 총 글수
 			bCount = new MemberManagementListService().getBoardCount();
 			//총 페이지수 = (총글개수 / 페이지당글수) + (총글개수에서 페이지당 글 수로 나눈 나머지가 0이 아니라면 나머지 페이지개수를 1 증가)
@@ -86,13 +86,24 @@ public class MemberManagementListServlet extends HttpServlet {
 			}
 			endPage = startPage + PAGE_BLOCK -1; 
 			if(endPage > pageCount) endPage=pageCount;
+//			System.out.println("후반: "+ searchKeyword);
+//			System.out.println("".equals(searchKeyword));
+			if ("".equals(searchKeyword) || searchKeyword == null ) {
+				ArrayList<Member> volist = new MemberManagementListService().selectBoardList(startRnum,endRnum);
+				request.setAttribute("boardvolist", volist);
+			}else {
+				ArrayList<Member> searchvolist = new MemberManagementListService().selectBoardList(vo);
+				request.setAttribute("boardvolist", searchvolist);
+			}
+			
+			
+			
 			
 			request.setAttribute("startPage", startPage);
 			request.setAttribute("endPage", endPage);
 			request.setAttribute("pageCount", pageCount);
 			//Page 이동하면서 Data도 전달함.
-			
-				request.setAttribute("searchKeyword", searchKeyword);
+			request.setAttribute("searchKeyword", searchKeyword);
 //				response.sendRedirect("서블릿url");
 				request.getRequestDispatcher("WEB-INF/Management/MemberManagementBoard.jsp").forward(request, response);
 			
